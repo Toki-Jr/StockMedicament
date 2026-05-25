639e234afd9ebeb73d6f6d43c259c23669a33fec
Toki-Jr <robertrandrianandrasana62@gmail.com>
2026-05-25 17:38:04 +0300

    feat: branche dédiée au frontend sans le dossier server

--- server/src/routes/dashboard.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const ctrl   = require('../controllers/dashboard.controller');
-const { authenticate } = require('../middlewares/auth.middleware');
-
-router.get('/', authenticate, ctrl.getStats);
-router.get('/commandes-par-jour', authenticate, ctrl.getCommandesParJour);
-
-module.exports = router;
