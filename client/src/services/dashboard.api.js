d0c323810fc97af92f6c67ef8395869b2ffde6cb
Toki-Jr <robertrandrianandrasana62@gmail.com>
2026-05-25 17:36:41 +0300

    feat: branche dédiée au backend sans le dossier client

--- client/src/services/dashboard.api.js
+++
@@ -1,4 +0,0 @@
-import api from "./axios.instance";
-export const getDashboardStats = () => api.get('/dashboard').then(r => r.data);
-export const getCommandesParJour = () =>
-  api.get('/dashboard/commandes-par-jour').then(r => r.data);
